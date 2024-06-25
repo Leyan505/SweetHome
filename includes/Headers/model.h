36c6a89a8e2bfe96994fe4675359bcc94b249907
caelruz4 <carely.cruz.mendoza@gmail.com>
2024-06-24 18:43:00 -0600

    Add living room model

--- includes/Headers/model.h
+++ includes/Headers/model.h
@@ -11,7 +11,6 @@
 #include <assimp/postprocess.h>
 
 #include "mesh.h"
-
 #include "shader_m.h"
 
 #include <string>
@@ -202,7 +201,6 @@ private:
     }
 };
 
-
 unsigned int TextureFromFile(const char *path, const string &directory, bool gamma)
 {
     string filename = string(path);
@@ -214,7 +212,8 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
     unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
     if (data)
     {
-        GLenum format;
+        GLenum format = GL_RGB; // Inicialización predeterminada
+
         if (nrComponents == 1)
             format = GL_RED;
         else if (nrComponents == 3)
